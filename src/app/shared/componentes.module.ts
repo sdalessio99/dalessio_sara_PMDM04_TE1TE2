fa9975751ce704a9997142ebc5bc68abc386d578
Sara D'Alessio <sdalessio@birt.eus>
2024-03-01 18:08:59 +0100

    commit ejercicio 4

--- src/app/shared/componentes.module.ts
+++ src/app/shared/componentes.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { HeaderTabComponent } from './header-tab/header-tab.component';
+import { ExploreContainerComponent } from './explore-container/explore-container.component';
 
 
 
 @NgModule({
-  declarations: [HeaderTabComponent],
+  declarations: [HeaderTabComponent, ExploreContainerComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [HeaderTabComponent]
+  exports: [HeaderTabComponent, ExploreContainerComponent]
 })
 export class ComponentesModule { }
--- src/app/tabs/tab2/tab2.module.ts
+++ src/app/tabs/tab2/tab2.module.ts
@@ -3,17 +3,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
-import { ComponentesModule } from 'src/app/shared/componentes.module';
+import { ComponentesModule } from '../../shared/componentes.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     Tab2PageRoutingModule,
     ComponentesModule
   ],
